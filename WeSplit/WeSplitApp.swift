87cb85d8b47aeb40b3fe922c500ca8da51c6dfc9
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-01 18:32:01 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Brayan Duwe on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
