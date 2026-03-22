d355ca0c079d0633e94f4e40162671a119b4aaed
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-22 11:10:55 -0300

    Segmented Picker

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -3,10 +3,10 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPercentage = 20
+    @State private var tipPercentage = 15
     @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [0, 10, 15, 20, 25]
+    @State private var tipPercentages = [0, 10, 15, 20, 25]
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
@@ -43,10 +43,10 @@ struct ContentView: View {
                 
                 Section("How much do you want to tip?") {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(0..<100) {
-                            Text($0, format: .percent)
+                        ForEach(tipPercentages, id: \.self) { tips in
+                            Text(tips, format: .percent)
                         }
-                    } .pickerStyle(.navigationLink)
+                    } .pickerStyle(.segmented)
                 }
                 
                 Section("Amount per person") {
